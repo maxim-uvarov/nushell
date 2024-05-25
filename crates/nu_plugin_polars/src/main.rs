b31cbde962414ee87a0ece8add04113670ed8408
Maxim Uvarov <maxim-uvarov@users.noreply.github.com>
2024-05-25 12:03:39 +0000

    try to copy changes from `dply`

--- crates/nu_plugin_polars/src/main.rs
+++ crates/nu_plugin_polars/src/main.rs
@@ -1,6 +1,13 @@
 use nu_plugin::{serve_plugin, MsgPackSerializer};
 use nu_plugin_polars::PolarsPlugin;
 
+#[cfg(not(target_env = "msvc"))]
+use jemallocator::Jemalloc;
+
+#[cfg(not(target_env = "msvc"))]
+#[global_allocator]
+static GLOBAL: Jemalloc = Jemalloc;
+
 fn main() {
     serve_plugin(&PolarsPlugin::default(), MsgPackSerializer {})
 }
